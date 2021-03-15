81f96d55bcd6db2586a8157c49c8ce5114b1cb27
Jacob S <jacob.g.stewart@gmail.com>
2021-03-15 11:31:45 -0400

    adding bookmark feature

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ menuButton.addEventListener('click', () => {
   const menuModal = document.querySelector('.menu-modal');
   menuModal.classList.toggle('hidden');
 });
+
+
+// Bookmark
+const bookmarkContainer = document.querySelector('.bookmark-container');
+bookmarkContainer.addEventListener('click', e => {
+  const bookmarkButton = document.querySelector('.bookmark-button');
+  const bookmarkIcon = document.querySelector('.bookmark-icon');
+
+  bookmarkButton.classList.toggle('active');
+  bookmarkIcon.classList.toggle('active');
+
+  console.log(bookmarkButton.classList);
+
+  if ([...bookmarkButton.classList].includes('active')) {
+    bookmarkButton.innerHTML = 'Bookmarked';
+  } else {
+    bookmarkButton.innerHTML = 'Bookmark';
+  }
+});
