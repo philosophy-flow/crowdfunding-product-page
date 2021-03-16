fc660d0efb5f34daf0e1b258af9484a982554e88
Jacob S <jacob.g.stewart@gmail.com>
2021-03-16 13:09:03 -0400

    adding active state change on radio input change

--- script.js
+++ script.js
@@ -36,15 +36,49 @@ const bambooButton = document.querySelector('.bamboo-button');
 const blackButton = document.querySelector('.black-button');
 const closeModal = document.querySelector('.pledge-modal-close-btn');
 
+const noRewardInput = document.getElementById('no-reward-selected');
+const bambooInput = document.getElementById('bamboo-selected');
+const blackInput = document.getElementById('black-selected');
+
+const noRewardBox = document.querySelector('.no-reward');
+const bambooRewardBox = document.querySelector('.bamboo-reward');
+const blackRewardBox = document.querySelector('.black-reward');
+
+
+const rewardInputArr = [bambooInput, blackInput, noRewardInput];
+const rewardBoxArr = [noRewardBox, bambooRewardBox, blackRewardBox];
+
+
 bambooButton.addEventListener('click', () => {
   rewardModal.classList.remove('hidden');
+  bambooInput.checked = true;
+  bambooRewardBox.classList.add('active');
 });
 
 blackButton.addEventListener('click', () => {
   rewardModal.classList.remove('hidden');
+  blackInput.checked = true;
+  blackRewardBox.classList.add('active');
 });
 
 closeModal.addEventListener('click', e => {
   e.preventDefault();
   rewardModal.classList.add('hidden');
+  rewardBoxArr.forEach(box => {
+    if (box.classList.contains('active')) {
+      box.classList.remove('active');
+    }
+  });
+});
+
+rewardInputArr.forEach(input => {
+  input.addEventListener('change', () => {
+    rewardBoxArr.forEach(box => {
+      if (box.classList.contains('active')) {
+        box.classList.remove('active');
+      }
+    });
+    const newActiveBox = input.parentElement.parentElement.parentElement;
+    newActiveBox.classList.add('active');
+  });
 });
